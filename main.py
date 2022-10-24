a591ab39e2ee9a59ba9c12bf7d3028a45322a451
john <john.geroitos@gmail.com>
2022-10-25 02:28:23 +0300

    - Added Quote class - Quote class builds itself on first call

--- main.py
+++ main.py
@@ -1,60 +1,73 @@
+import os
+
 import nltk
 import random
 import re
-import img_downloader
+import downloader
 import logging
+import manipulation
+# import messages  # Todo: add messages for console input menu
 
 # nltk.download('averaged_perceptron_tagger')
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 level = 'DEBUG'
 
 
-class Quote():
+class Quote:
     def __init__(self, text=None, author=None):
         self.text = text
         self.author = author
+        self.nouns = []
+        self.verbs = []
+        self.adjectives = []
+        self.banned = ['person', 'face']
+        self.get_quote()
 
 
     def get_quote(self):
         # Get a random quote from quotes.txt
         with open('quotes.txt', 'r') as f:
             quotes = f.readlines()
-        return quotes[random.randint(0, len(quotes))]
+        self.prep_text(quotes[random.randint(0, len(quotes))])
+        return self.text, self.author
 
-    def sep_author_text(self, text):
-        # Separates a text of form quote -Author
+    def prep_text(self, text):
+        # Separates a text of form quote -Author and breaks it down to parts of speech
         self.author = [match.group(1) for match in re.finditer(r'[—―]([ .A-Za-z]+)', text)][0]
         self.text = [match.group(1) for match in re.finditer(r'(.+) *[—―]', text)][0].strip().replace('"', '')
 
+        self.nouns = self.get_pos('NN')
+        self.verbs = self.get_pos('VB')
+        self.adjectives = self.get_pos('JJ')
 
-
-# def build_inspiration():
-#     # Reads words from inspiring.txt and returns a lowercase list
-#     with open('inspiring.txt', 'r') as f:
-#         results = f.readlines()
-#     return [x.lower().strip() for x in results]
-
-def get_pos(text, pos):
-    is_pos = lambda part: part[:2] == pos
-    tokenized = nltk.wordpunct_tokenize(text)
-    results = [word for (word, pos) in nltk.pos_tag(tokenized) if is_pos(pos)]
-    return sorted([x for x in results if len(x) > 1], key=len, reverse=True)
-
-
-
+    def get_pos(self, pos):
+        # Returns a list with the words that are of the given part of speech
+        is_pos = lambda part: part[:2] == pos
+        tokenized = nltk.wordpunct_tokenize(self.text)
+        results = [word for (word, pos) in nltk.pos_tag(tokenized) if is_pos(pos)]
+        return sorted([x for x in results if len(x) > 1 and x.lower() not in self.banned], key=len, reverse=True)
 
 
 quote = Quote()
-quote.sep_author_text(quote.get_quote())
 logging.info(f'Selected: {quote.text} by {quote.author}')
-nouns = get_pos(quote.text, 'NN')
-verbs = get_pos(quote.text, 'VB')
-adjectives = get_pos(quote.text, 'JJ')
-search_str = ''
-search_str += nouns[0] if nouns else ''
-search_str += ' ' + verbs[0] if verbs else ''
-search_str += ' ' + adjectives[0] if adjectives else ''
-folder = img_downloader.download(search_str)
+# print(quote.text, quote.author, quote.nouns, quote.verbs)
+if quote.nouns:
+    if quote.verbs:
+        img_folder = downloader.download(quote.nouns[0] + ' ' + quote.verbs[0], 3)
+    else:
+        img_folder = downloader.download(quote.nouns[0], 3)
 
+image = manipulation.Img(random.choice(os.listdir(img_folder)), text=quote.text, author=quote.author)
+image.draw()
+
+# search_str = ''
+# search_str += quote.nouns[0] if quote.nouns else ''
+# search_str += ' ' + quote.verbs[0] if quote.verbs else ''
+# search_str += ' ' + quote.adjectives[0] if quote.adjectives else ''
+# folder = img_downloader.download(search_str)
+# file_list = os.listdir(folder)
+# path = file_list[random.randint(0, len(file_list))]
 # good_candidates = [x for x in quote.split() if x.lower() in inspirational]
 
+if __name__ == '__main__':
+    pass  # TODO: Add console menu
