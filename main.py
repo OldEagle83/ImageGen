5304769c0c0c10ccd4f46791c16818f5c7a027ab
john <john.geroitos@gmail.com>
2022-10-25 13:07:46 +0300

    Lemmatizer & docstrings

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import manipulation
 from nltk import WordNetLemmatizer
 # import messages  # Todo: add messages for console input menu
 
+# Comment these out after first run
 # nltk.download('averaged_perceptron_tagger')
 # nltk.download('omw-1.4')
 # nltk.download('wordnet')
 
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
 
 
 class Quote:
@@ -27,23 +27,24 @@ class Quote:
         self.banned = ['person', 'face']
         self.get_quote()
 
-
     def get_quote(self):
         # Get a random quote from quotes.txt
         with open('quotes.txt', 'r') as f:
             quotes = f.readlines()
-        self.prep_text(quotes[random.randint(0, len(quotes))])
+
+        self.prep_text(random.choice(quotes))
+
         return self.text, self.author
 
     def prep_text(self, text):
         # Separates a text of form quote -Author and breaks it down to parts of speech
+
         self.author = [match.group(1) for match in re.finditer(r'[—―]([ .A-Za-z]+)', text)][0]
         self.text = [match.group(1) for match in re.finditer(r'(.+) *[—―]', text)][0].strip().replace('"', '')
 
         self.nouns = self.get_pos('NN')
         self.verbs = self.get_pos('VB')
         self.adjectives = self.get_pos('JJ')
-
 
     def get_pos(self, pos):
         # Returns a list with the words that are of the given part of speech
@@ -53,7 +54,7 @@ class Quote:
         if pos == 'VB':
             results = [WordNetLemmatizer().lemmatize(word, 'v') for word in results]
         results = sorted([x for x in results if len(x) > 1 and x.lower() not in self.banned], key=len, reverse=True)
-        logging.debug(results)
+        logging.debug(f'{pos}: {results}')
         return results
 
 
@@ -68,15 +69,6 @@ if quote.nouns:
 
 image = manipulation.Img(img_folder + '/' + random.choice(os.listdir(img_folder)), text=quote.text, author=quote.author)
 image.draw()
-
-# search_str = ''
-# search_str += quote.nouns[0] if quote.nouns else ''
-# search_str += ' ' + quote.verbs[0] if quote.verbs else ''
-# search_str += ' ' + quote.adjectives[0] if quote.adjectives else ''
-# folder = img_downloader.download(search_str)
-# file_list = os.listdir(folder)
-# path = file_list[random.randint(0, len(file_list))]
-# good_candidates = [x for x in quote.split() if x.lower() in inspirational]
 
 if __name__ == '__main__':
     pass  # TODO: Add console menu
